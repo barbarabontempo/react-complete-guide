a054ea52cac126913b9118e93e9c399bf3430ef9
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-12-22 10:04:22 -0500

    inline styling or seperate css file

--- src/App.js
+++ src/App.js
@@ -1,39 +1,131 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person"; //can omit the .js because its added automatically by the build workflow
 
 class App extends Component {
+  //state is managed from inside of a component
+  //can only use state in class based component
+  //can manage state in funcitonal components only with hooks
+  state = {
+    persons: [
+      {
+        name: "barbs",
+        age: "26",
+      },
+      {
+        name: "kc",
+        age: "26",
+      },
+      {
+        name: "fran",
+        age: "26",
+      },
+    ],
+    //state is a special property, it can be changed
+    //if it changes, it will lead React to re-render our DOM, to update the DOM
+  };
+
   //this class has a render method
   //we need this method, because react will call this method to render something on the screen
   //it has to return or render some HTML code to the DOM
 
+  switchNameHandler = () => {
+    //console.log("was clicked")
+
+    //DONT DO THIS: this.state.persons[0].name = "BARBI"
+    //use special method from react that allows us to update state
+    //takes obj as argument
+    this.setState({
+      persons: [
+        {
+          name: "BARBI",
+          age: "26",
+        },
+        {
+          name: "kc",
+          age: "26",
+        },
+        {
+          name: "fran",
+          age: "36",
+        },
+      ]
+    })
+  }
+
+
+  nameChangedHandler = (e) => {
+    this.setState({
+      persons: [
+        {
+          name: "BARBI",
+          age: "26",
+        },
+        {
+          name: e.target.value,
+          age: "26",
+        },
+        {
+          name: "fran",
+          age: "36",
+        },
+      ]
+    })
+  }
+
   render() {
+    // styling inline
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
-      // the code in here, is JSX (JS looking a bit different)
-      // this is just syntatical sugar invented by react team
-      // will transpile to valid JS
-      // code that we can use to write HTML 
+      //the code in here, is JSX (JS looking a bit different)
+      //this is just syntatical sugar invented by react team
+      //will transpile to valid JS
+      //code that we can use to write HTML
       <div className="App">
-       <h1> Hello, I am a react app</h1>
-       <p>working! woo!</p>
-       <Person />
+        <h1> Hello, I am a react app</h1>
+        <p>working! woo!</p>
+        {/* inline styling for the button */}
+        <button
+        style={style} 
+        onClick={this.switchNameHandler} >Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}  
+          changed={this.nameChangedHandler}
+        >
+          {" "}
+          My hobbies are: painting{" "}
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
       //best practice is to wrap everything in a root element
       //typically u net everything inside an element you return
 
-          //create element is a method that takes at least 3 argument
-          //first one is element we want to render to the DOM 
-          //second argument, configuation for this, we would pass a JS object
-          //third argument is any amount of children
-          //children = what is nested inside this div, in this caase we want to nest h1 inside the div
-          //this code below, is the same thing as the code above, this is why we need to import react
-          //writing code like this below is cumbersome, which is why we use JSX like above
-          //but the code below is what is happening behind the scene, the code above looks like HTML, but in erality it gets compiled like the code below
-        // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, "I AM A REACT APP"))
+      //create element is a method that takes at least 3 argument
+      //first one is element we want to render to the DOM
+      //second argument, configuation for this, we would pass a JS object
+      //third argument is any amount of children
+      //children = what is nested inside this div, in this caase we want to nest h1 inside the div
+      //this code below, is the same thing as the code above, this is why we need to import react
+      //writing code like this below is cumbersome, which is why we use JSX like above
+      //but the code below is what is happening behind the scene, the code above looks like HTML, but in erality it gets compiled like the code below
+      // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, "I AM A REACT APP"))
     );
   }
 }
 
 export default App;
-
-   
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import './Person.css'
 //in its simplest form a component is just a function that returns some JSX/HTML
 //we are using ES6, we will create function using const keyword and arrow function
 
+//props: attributes you add to your component
+//They are read-only components which must be kept pure i.e. immutable. 
+//They are always passed down from the parent to the child components throughout the application.
+//A child component can never send a prop back to the parent component. 
+//This help in maintaining the unidirectional data flow and are generally used to render the dynamically generated data
 
-const person = () => {
+const Person = (props) => {
 
-    return <p> I am a person</p>
+    return (
+        <div className="Person"> 
+            <p onClick={props.click}> I am {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p> 
+            {/* children any element between the opneing and closing tag of our component.  */}
+            <input type="text" onChange={props.changed} value={props.name}/> 
+            {/* onChange fired whenever the value iin the input changes */}
+        </div>
+    )
+    //if you have dynamic content in JSX, which we want to run as JS code and not as text, we need to wrap it in single curly brackets {}
+    //we can execute short simple expressions 
+    //you can call a function here that does more complex stuff
+    //super reusable code 
+
+    
 }
 
-export default person 
+export default Person 
