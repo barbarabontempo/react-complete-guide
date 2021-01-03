d362e14b201ac917f68da0a7fdbc9292776a946e
BarbaraBontempo <barbarabontempo94@gmail.com>
2021-01-03 14:29:54 -0500

    learning useEffect hook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
 const cockpit = ( props ) => {
+
+  //useEffect takes a function taht will run for every render cycle
+  useEffect(() => {
+    console.log('[cockpit.js], useEffect')
+    //http request
+    setTimeout(() => {
+      alert("saved data to cloud!")
+    }, 1000 );
+  }, [props.persons])
+
+  //useEffect will run all the time, combines CDM and CDU
+  //what if we want to send an HTTP request but we only want to do that when the comp is rendered for the FIRST time and not every re-render cycle.. useEffect with out the second argument, will run at every rerender cycle
+  
+  //this should only execute when our Persons change: how do we do this? add second argument to ueEffect
+  //am array where u point to all the variables/data that are used in your Effect
+
+  //so we only want it to execute when persons change so inside an array we pass: [props.person]
+
+
+  //what is we only want to execute useEffect when the component renders the first time?
+  //we pass an empty array! u have to pass an array but empty it tells React that this effect has no dependencies so it will only run the first time. 
+
+  //if you have a dependency on a certain field, then u would pass it onto the array like we did for props.person 
+  
+  
+
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,33 +5,47 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log('[App.js] constructor');
-  }
+// this will execute the constructor of the component you're extending
+
+constructor(props){
+  super(props)
+  console.log('[App.js] constructor')
+  
+}
 
   state = {
     persons: [
       { id: 'asfa1', name: 'Barb', age: 28 },
       { id: 'vasdf1', name: 'Karem', age: 29 },
-      { id: 'asdf11', name: 'Fran', age: 26 }
+      { id: 'asdf11', name: 'Frantelope', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   };
 
-  static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
+  static getDerivedStateFromProps(props, state) { // Rarely Used
+    console.log('[App.js] getDerivedState', props)
     return state;
   }
+  
+  // componentWillMount(){
+  //   console.log('[App.js] componentWillMount')
+  // }
 
-  componentWillMount() {
-    console.log('[App.js] componentWillMount');
+  componentDidMount(){
+    console.log('[App.js] componentDidMount')
   }
 
-  componentDidMount() {
-    console.log('[App.js] componentDidMount');
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate')
+    return true; //this allows for the update
   }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate')
+  }
+  
+
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -52,6 +66,35 @@ class App extends Component {
     this.setState({ persons: persons });
   };
 
+  //another way to update: 
+  //update an item in an array one attribute instead of replacing the whole object:
+  // nameChangedHandler = (evt, id) => {
+  //   // use map to return a new array so we aren't mutating state
+  //   const updatedPersons = this.state.persons.map(p => {
+  //     // in the array, look for the object we want to update
+  //     if (p.id === id){ // if we find the object
+  //       const updatedPerson = {...p} // make a copy of it
+  //       updatedPerson.name = evt.target.value // update whatever attribute that has changed
+  //       return updatedPerson // return the updated copy
+  //     } else { // for all other objects in the array
+  //       return p // return the original object
+  //     }
+  //   })
+  //   this.setState({ // set state with our updated array
+  //     persons: updatedPersons
+  //   });
+  // };
+
+  // another way to delete: 
+  // deletePersonHandler = (id) => {
+  //   //this is mutating state directly, dont use this
+  //   // const persons = this.state.persons
+  //   const updatedPersons = this.state.persons.filter(
+  //     (person) => person.id !== id
+  //   );
+  //   this.setState({ persons: updatedPersons });
+  // };
+
   deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
@@ -65,8 +108,8 @@ class App extends Component {
   };
 
   render() {
-    console.log('[App.js] render');
-    let persons = null;
+    console.log('[App.js] render')
+  let persons = null
 
     if (this.state.showPersons) {
       persons = (
